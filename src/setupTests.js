acd4f360c21580724ec2799f3cda9c2336796650
Karent Pinzon <karentypinzonf@corteconstitucional.gov.co>
2026-02-20 11:27:50 -0500

    se quita la repeticion de codigo

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,3 @@ import '@testing-library/jest-dom';
 if (!global.TextEncoder) {
     global.TextEncoder = require('util').TextEncoder;
 }
-if (!global.TextDecoder) {
-    global.TextDecoder = require('util').TextDecoder;
-}
